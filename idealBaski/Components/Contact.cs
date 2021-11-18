336653b1b5ee073b033e9140bf74afe7266cbffc
onur  umutluoglu <onurumutluoglu@gmail.com>
2021-11-18 18:24:11 +0300

    Simple Printing Press web page
    Simple Printing Press web page

---
+++ idealBaski/Components/Contact.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace idealBaski.Components
+{
+    public class Contact:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+
+        }
+    }
+}
---
+++ idealBaski/Controllers/ServicesController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace idealBaski.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Roll()
+        {
+            return View();
+        }
+        public IActionResult Folya()
+        {
+            return View();
+        }
+        public IActionResult Canvas()
+        {
+            return View();
+        }
+        public IActionResult Mesh()
+        {
+            return View();
+        }
+        public IActionResult One()
+        {
+            return View();
+        }
+        public IActionResult Refletif()
+        {
+            return View();
+        }
+
+        public IActionResult Kumlama()
+        {
+            return View();
+        }
+        public IActionResult orumcek()
+        {
+            return View();
+        }
+        public IActionResult Forex()
+        {
+            return View();
+        }
+        public IActionResult Renkli()
+        {
+            return View();
+        }
+        public IActionResult Raket()
+        {
+            return View();
+        }
+        public IActionResult Vinil()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
